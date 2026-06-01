a31cd25f908761b7aced843ef74f3af1c4cf2ac5
kajianaLeVrai999 <hei.kajiana@gmail.com>
2026-06-01 16:28:30 +0300

    fix: update resource

--- employes-admin/src/App.tsx
+++ employes-admin/src/App.tsx
@@ -1,20 +1,33 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { EmployeeList } from './employees/EmployeeList.tsx';
-import { EmployeeCreate } from './employees/EmployeeCreate.tsx';
-import { EmployeeEdit } from './employees/EmployeeEdit.tsx';
-import { EmployeeShow } from './employees/EmployeeShow.tsx';
+import { EmployeeList } from './employees/EmployeeList';
+import { EmployeeCreate } from './employees/EmployeeCreate';
+import { EmployeeEdit } from './employees/EmployeeEdit';
+import { EmployeeShow } from './employees/EmployeeShow';
+
+import { InternList } from './interns/InternList';
+import { InternCreate } from './interns/InternCreate';
+import { InternEdit } from './interns/InternEdit';
+import { InternShow } from './interns/InternShow';
+import { Dashboard } from './Dashboard';
 
 const dataProvider = jsonServerProvider('http://localhost:3002');
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource 
       name="employees" 
       list={EmployeeList} 
       create={EmployeeCreate} 
       edit={EmployeeEdit} 
       show={EmployeeShow} 
+    />
+    <Resource 
+      name="interns" 
+      list={InternList} 
+      create={InternCreate} 
+      edit={InternEdit} 
+      show={InternShow} 
     />
   </Admin>
 );
